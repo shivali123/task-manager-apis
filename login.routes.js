18cbe56f42f0b3bb20910dbf6dd9c3d5843169f8
shivali123 <40239357+shivali123@users.noreply.github.com>
2018-08-15 00:51:05 +0530

    Delete login.routes.js

--- login.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const login = require('../controllers/login.controllers.js');
-
-    app.get('/login/:email', login.findOne);
-    app.get('/login', login.findAll);
-}
