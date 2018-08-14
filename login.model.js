681864e9e5536ea1917f5eb7ce56802b679a80e6
shivali123 <40239357+shivali123@users.noreply.github.com>
2018-08-15 00:51:25 +0530

    Delete login.model.js

--- login.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const loginSchema = mongoose.Schema({
-    password: String,
-    email:  String
-    });
-module.exports = mongoose.model('login', loginSchema);
